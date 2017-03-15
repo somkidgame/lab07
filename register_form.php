be2dd7aee3ceba571262f72a7efae290994e43aa
somkidgame <gamezanakup23@gmail.com>
2017-03-15 21:22:36 +0700

    edit01

--- register_form.php
+++ register_form.php
@@ -1,5 +1,63 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Form Validation</title>\
+<title>Form Validation</title>
 </head>
+<body>
+<h3>แบบฟอร์มลงทะเบียน</h3>
+<form action="dopost.php" method="post" class="a">
+ชื่อ-นามสกุล: <br/>
+<input type="text" class="text" name="name" id="name" /> <br/>
+อีเมลล์: <br/>
+<input type="text" class="text" name="name" id="email" /> <br/>
+เพศ: <br/>
+<input type="radio" class="radio" name="sex" id="sex" value="M" /> ชาย
+<input type="radio" class="radio" name="sex" id="sex" value="F" /> หญิง
+<br />
+ความสนใจ: <br/>
+<input type="checkbox" class="checkbox" name="intr1" id="intr1" /> เรียน
+<input type="checkbox" class="checkbox" name="intr2" id="intr2" /> เกมส์
+<br />
+ที่อยู่: <br/>
+<textarea class="text" name="address" id="address" rows="4" cols="50" ></textarea>
+<br />
+จังหวัด: <br/>
+<select name="province" id="province">
+<option value="">---เลือกจังหวัด---</option>
+<option value="1">กรุงเทพมหานครฯ</option>
+<option value="2">ชลบุรี</option>
+</select><br />
+
+<br/><br/>
+<input type="submit" id="submit" value="Submit" name="submit" />
+</form>
+
+<script src="https://code.jquery.com/jquery-2.1.1min.js"></script>
+<script>
+$('#submit').on('click',function(event) {
+    var valid = true;
+    errorMessage = "";
+
+    if($('#name').val() == '') {
+        errorMessage = "โปรดป้อนชื่อ-นามสกุล \n";
+        valid = false;
+    }
+
+    if($('#address').val() == '') {
+        errorMessage += "โปรดป้อนที่อยู่\n";
+        valid = false;
+    }
+
+    if($('#email').val() == '') {
+        errorMessage += "โปรดป้อน email\n";
+        valid = false;
+    }
+
+    if( !valid && errorMessage.length > 0) {
+        alert(errorMessage);
+        event.proventDefault();
+    }
+});
+</script>
+</body>
+</html>
